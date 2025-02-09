703771da8ab7256ab554a70541b212b4ecae852a
allis1infinity <igorinna35@gmail.com>
2025-02-09 23:24:10 +0100

    file creating

---
+++ js/task-3.js
@@ -0,0 +1,13 @@
+function filterArray(numbers, value) {
+  const totalErray = [];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > value) totalErray.push(numbers[i]);
+  }
+  return totalErray;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
